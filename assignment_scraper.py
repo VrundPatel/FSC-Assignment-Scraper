05877462501e1065dde8d6e797781cb158a22ee6
Vrund Patel <vrundp07@yahoo.com>
2016-08-22 23:07:49 -0400

    Changed the url to take directly to the students section of the portal

--- assignment_scraper.py
+++ assignment_scraper.py
@@ -2,6 +2,7 @@ import time
 import argparse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 #reading the passed username and password in the command line.
 parser = argparse.ArgumentParser()
@@ -9,11 +10,13 @@ parser.add_argument('username', nargs='+')
 parser.add_argument('password', nargs='+')
 args = parser.parse_args()
 
-#intializing the Chrome webdriver.
+#Intializing the Chrome webdriver.
 driver = webdriver.Chrome()
+#Initializing the actions driver.
+actions = ActionChains(driver)
 
 #Opening the url for the portal.
-driver.get("https://portal.flsouthern.edu/ics")
+driver.get("https://portal.flsouthern.edu/ICS/Students/")
 
 #Entering the username and password for the user for login.
 elem = driver.find_element_by_id("userName")
@@ -25,9 +28,13 @@ elem.send_keys(args.password)
 elem = driver.find_element_by_id("btnLogin")
 elem.send_keys(Keys.RETURN)
 
+# course = driver.find_element_by_id("pg1_V_rptrCourses_ctl01_rptrItems_ctl01_hlCourseCode")
+# print("found course")
+# actions.click(course)
+driver.get("https://portal.flsouthern.edu/ICS/Academics/CSC/CSC__3310/2016_DF-CSC__3310-001/")
 #Getting the courses for the user.
-elem = driver.find_element_by_id("myCourses")
-elem.send_keys()
+# elem = driver.find_element_by_id("myCourses")
+# elem.send_keys()
 # driver.close()
 
 #writing the assignments to a file.
