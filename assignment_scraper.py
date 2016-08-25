e8a629abdf2b47e3f0811bd3a2645c425739b3f4
Vrund Patel <vrundp07@yahoo.com>
2016-08-25 11:09:36 -0400

    Getting credentials from a text file

--- assignment_scraper.py
+++ assignment_scraper.py
@@ -1,14 +1,12 @@
 import time
-import argparse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-#reading the passed username and password in the command line.
-parser = argparse.ArgumentParser()
-parser.add_argument('username', nargs='+')
-parser.add_argument('password', nargs='+')
-args = parser.parse_args()
+# Reading credentials from the text file
+file = open("credentials.txt", 'r')
+username = file.readline()
+password = file.readline()
 
 #Intializing the Chrome webdriver.
 driver = webdriver.Chrome()
@@ -22,10 +20,10 @@ driver.get("https://portal.flsouthern.edu/ICS/Students/")
 #Entering the username and password for the user for login.
 elem = driver.find_element_by_id("userName")
 elem.clear()
-elem.send_keys(args.username)
+elem.send_keys(username)
 elem = driver.find_element_by_id("password")
 elem.clear()
-elem.send_keys(args.password)
+elem.send_keys(password)
 elem = driver.find_element_by_id("btnLogin")
 elem.send_keys(Keys.RETURN)
 
@@ -42,12 +40,10 @@ driver.get("https://portal.flsouthern.edu/ICS/Academics/REL/REL__1108/2016_DF-RE
 for i in range(100, 105):
     driver.find_element_by_class_name("assignmentText")
     id = "pg0_V__assignmentView__rptAssignments_ctl00__studentAssignBody__rptAssignments_ct" + str(i) + "__hyAssign"
-    elem = driver.find_element_by_id(id)
+    elem = driver.find_elements_by_id(id)
     print(elem.text)
 
-
-
-#writing the assignments to a file.
+#   writing the assignments to a file.
 # with open("assignments.csv", "a") as csv_file:
 #     csv_file.write(' '.join())
 #     csv_file.close()
